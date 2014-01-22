290e9bf631b632236d5e7773ff81b00f8b8ebe02
Dmitry Wolf <dmitry.s.wolf@gmail.com>
2014-01-23 00:16:02 +0400

    something

--- osx/osx_link_dotfiles.rb
+++ osx/osx_link_dotfiles.rb
@@ -5,14 +5,19 @@ puts "Linking library ..."
 DOTFILES = "~/.dotfiles"
 
 links = [
-	"osx/etc/.bashrc",
+
 	"osx/etc/.htoprc",
-	"osx/etc/.zshrc",
-	"etc/git/.gitattributes",
-	"etc/git/.gitconfig",
-	"etc/git/.gitignore",	
+
+	# shell
+	"osx/etc/.bashrc",	
+	"osx/etc/.zshrc",		
 	"osx/logs/.bash_history",
-	"osx/logs/.zsh_history"
+	"osx/logs/.zsh_history",
+	
+	# git
+	"osx/etc/git/.gitconfig",
+	"etc/git/.gitattributes",
+	"etc/git/.gitignore"
 ]
 
 links.each	do |link|
